2997dc7757e6234faed4625424cda4c7bc7789fb
Gyaneswar Singh <gyaneswarsingh53@gmail.com>
2021-06-22 21:04:14 +0530

    Divisors of a number

---
+++ Mathematics/DivisorsOfANumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class DivisorsOfANumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                System.out.println(i);
+                if(i!=n/i)
+                    System.out.println(n/i);
+            }
+        }
+    }
+}
