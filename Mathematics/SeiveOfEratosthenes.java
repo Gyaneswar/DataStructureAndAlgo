b2c71236187597f646d8727b542c6c1e81429f72
Gyaneswar Singh <gyaneswarsingh53@gmail.com>
2021-06-22 21:47:12 +0530

    seive of eratosthenes

--- Mathematics/SeiveOfEratosthenes.java
+++ Mathematics/SeiveOfEratosthenes.java
@@ -1,1 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 // this algo is used to find all the prime numbers which comes before N
+public class SeiveOfEratosthenes{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        boolean isPrime[]=new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        isPrime[0]=false;
+        isPrime[1]=false;
+
+        for(int i=2;i*i<=n;i++){
+            if(isPrime[i]){                                
+                for(int j=2*i;j<=n;j=j+i){                                                            
+                    isPrime[j]=false;
+                }
+            }
+        }
+
+        for(int i=2;i<=n;i++){
+            if(isPrime[i])
+                System.out.println(i);
+        }
+    }
+}
