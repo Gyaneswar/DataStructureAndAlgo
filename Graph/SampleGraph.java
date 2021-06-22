cb8445579a5df521385a0feb1a8ae2c3ca60a519
Gyaneswar Singh <gyaneswarsingh53@gmail.com>
2021-06-22 21:07:32 +0530

    test commit

--- Graph/SampleGraph.java
+++ Graph/SampleGraph.java
@@ -1,5 +1,15 @@
 public class SampleGraph {
     public static void main(String[] args) {
-        
+        //there are basically two types of graph directed and undirected graph
+
+
+
     }   
+
+
+    //This class represents each Node in a graph, in a directed graph each node should be pointing to another node while in 
+    //undirected grap
+    class Node{
+
+    }
 }
