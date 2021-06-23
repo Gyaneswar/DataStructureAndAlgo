75b87b8425df1be2da5e3513ac5cf2596f16c8db
Gyaneswar Singh <gyaneswarsingh53@gmail.com>
2021-06-23 15:56:51 +0530

    prime factors

--- Mathematics/PrimeFactors.java
+++ Mathematics/PrimeFactors.java
@@ -3,24 +3,15 @@ public class PrimeFactors {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        int n=sc.nextInt();
-        int x=n;
-        for(int i=2;i*i<=n;i++){
-            if(IsPrime(i)){
-                while(x%i==0){
+        int n=sc.nextInt();        
+        for(int i=2;i*i<=n;i++){            
+                while(n%i==0){
                     System.out.println(i);
-                    x=x/i;
-                }
-            }
+                    n=n/i;
+                }            
         }
-    }
-
-
-    public static boolean IsPrime(int n){
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0)
-                return false;
-        }
-        return true;
+        if(n>1)
+        System.out.println(n);
     }
 }
+//time complexity : O(sqrt(n))
