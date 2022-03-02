ff4612acec9b7362390c82510016be956f0cf488
Gyaneswar Singh <gyaneswarsingh53@gmail.com>
2022-03-02 20:13:14 +0530

    comment

--- Array/MoveZerosToEnd.java
+++ Array/MoveZerosToEnd.java
@@ -2,7 +2,7 @@ public class MoveZerosToEnd {
     public static void main(String[] args) {
         int n=sc.nextInt();
         int arr[] = new int[n];
-        //simple question cant be bothered to solve
+        //simple question cant be bothered to solve, just use 2 pointer method
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
