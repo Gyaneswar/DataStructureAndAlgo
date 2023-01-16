479b730f6b8f47c0ebd25be51bf97a3ab973de33
Gyaneswar Singh <gyaneswarsingh53@gmail.com>
2023-01-16 16:47:15 +0530

    remove unnecesary folder

--- LeetCode/Hard/MergeKSortedLists.java
+++
@@ -1,8 +0,0 @@
-//https://leetcode.com/problems/merge-k-sorted-lists/
-public class MergeKSortedLists {
-    public static void main(String[] args) {
-        
-    }
-
-    public static void main
-}
